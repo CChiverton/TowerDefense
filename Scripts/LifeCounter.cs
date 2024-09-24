fcd4d978de9c8743763c6290d297a51cc8f4bc10
CChiverton <75337792+CChiverton@users.noreply.github.com>
2024-09-24 15:22:58 +0100

    Life system added
    Lives are now in the game and are removed when an enemy enters the player zone

--- Scripts/LifeCounter.cs
+++ Scripts/LifeCounter.cs
@@ -1,21 +1,16 @@
 using Godot;
 using System;
 
-public partial class Game : Node2D
+public partial class LifeCounter : Label
 {
-	public int Lives = 100;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Lives = 100;
-		GetNode<LifeCounter>("UI/LifeCounter").SetLives(Lives);
 	}
-
-	private void OnPlayerZoneLifeLoss()
+	
+	public void SetLives(int lives)
 	{
-		Lives -= 1;
-		GetNode<LifeCounter>("UI/LifeCounter").SetLives(Lives);
+		Text = $"Lives: {lives}";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/PlayerZone.cs
+++ Scripts/PlayerZone.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class PlayerZone : Area2D
 {
+	[Signal]
+	public delegate void LifeLossEventHandler();
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,7 +14,7 @@ public partial class PlayerZone : Area2D
 	// Called whenever an enemy enters this zone
 	public void RemoveLife()
 	{
-		GD.Print("Removing Life");
+		EmitSignal(SignalName.LifeLoss);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
