1f81a792725aaa0cdf8632ba499d3279d7df0431
CChiverton <75337792+CChiverton@users.noreply.github.com>
2024-09-24 16:33:11 +0100

    Nearest Enemy targeting
    Enemies are put within a group which allows for easy iteration and targeting of which is nearest

--- Scripts/Tower.cs
+++ Scripts/Tower.cs
@@ -3,11 +3,36 @@ using System;
 
 public partial class Tower : Area2D
 {
+	private float _posX, _posY;
+	private float _attackRange = 150;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_posX = GlobalPosition.X;
+		_posY = GlobalPosition.Y;
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		Node2D NearestEnemy;
+		float NearestDistance = 1000;
+
+		foreach (Node2D enemy in GetTree().GetNodesInGroup("Enemies"))
+		{
+			float Distance = Position.DistanceTo(enemy.GlobalPosition);
+
+			if (Distance < NearestDistance)
+			{
+				NearestDistance = Distance;
+				NearestEnemy = enemy;
+			}
+		}
+		if (NearestDistance < _attackRange)
+		{
+			GD.Print("Enemy Within Range");
+			// Fire at enemy
+		}
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
