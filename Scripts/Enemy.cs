345e0ee0c93473c9e5db185545cda01d386abc0a
CChiverton <75337792+CChiverton@users.noreply.github.com>
2024-09-24 14:16:45 +0100

    Basic path added
    A Path2d and PathFollow2D have been added and enemies can successfully follow the path

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Enemy : Area2D
 {
+	private PathFollow2D _path;
+	private float _speed = 50.0F;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_path = GetParent<PathFollow2D>();
+	}
+
+	private void Follow(double delta)
+	{
+		_path.SetProgress(_path.GetProgress() + _speed * (float)delta);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Follow(delta);
 	}
 }
